10990a4a9a5b62369fe71b70a09dc86652adfebf
DulcheE <34923261+DulcheE@users.noreply.github.com>
2020-11-09 23:02:08 +0100

    Update test.vue

--- pages/test.vue
+++ pages/test.vue
@@ -13,14 +13,14 @@
               </div>
               <div>
                 <v-icon small>
-                  {{ universe.bIsPublic ? 'mdi-book-lock-open-outline' : 'mdi-book-lock-outline' }}
+                  {{ universe.public ? 'mdi-book-lock-open-outline' : 'mdi-book-lock-outline' }}
                 </v-icon>
               </div>
             </div>
           </v-container>
         </v-card-title>
         <v-card-subtitle>
-          <i>owner</i>
+          <i>{{ universe.user }}</i>
         </v-card-subtitle>
         <v-card-text>
           {{ universe.description }}
@@ -46,12 +46,24 @@ export default {
   computed: {
   },
   mounted () {
-    traverson
-      .from('http://localhost:3000/api')
+    traverson.from('http://localhost:3000/api/v1')
       .follow('$._links.universes')
       .getResource().result
       .then((document) => {
         this.universes = document.universes
+        return Promise.all(this.universes.map((universe) => {
+          console.log('here')
+          console.log(universe._links.user)
+          traverson.from(universe._links.user)
+            .getResource().result
+            .then((document) => {
+              console.log('here2')
+              universe.user = document
+            })
+        }))
+      })
+      .then(() => {
+        console.log(this.universes)
       })
       .catch((err) => {
         throw err.message
