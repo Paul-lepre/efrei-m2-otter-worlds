7c02c771100afc21fb530d78572f2146b4c7e1cc
scorpionsdu78 <fboni.78@gmail.com>
2020-11-12 14:55:05 +0100

    login proto

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,6 +6,7 @@ import putUser from '../controllers/user/put/put.user.js'
 import deleteUser from '../controllers/user/delete/delete.user.js'
 import putPassword from '../controllers/user/put/put.user.password.js'
 import getUserCharacter from '../controllers/user/get/get.user.characters'
+import login from '../controllers/user/post/post.login'
 import getUserUniverse from '../controllers/user/get/get.user.universes'
 const router = Router()
 
@@ -14,6 +15,7 @@ router.get('/:id', getUser)
 router.get('/:id/characters', getUserCharacter)
 router.get('/:id/universes', getUserUniverse)
 router.post('/', postUser)
+router.post('/login', login)
 router.put('/', putUser)
 router.put('/password/:id&:code', putPassword)
 router.delete('/', deleteUser)
