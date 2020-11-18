9630c94db0d2348fe088e925c1aefdc25b5c74d5
Paul-lepre <paul.lepre@efrei.net>
2020-11-18 16:31:00 +0100

    18/11/2020 profile

---
+++ pages/myprofile.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container>
+    <h1 class="primary--text">
+      Name / Password
+    </h1>
+    <v-container>
+      <h4>
+        username
+      </h4>
+      <v-form v-model="valid">
+        <v-container>
+          <v-row>
+            <v-col
+              cols="12"
+              md="4"
+            >
+              <v-text-field
+                v-model="password"
+                :rules="passwordRules"
+                :counter="10"
+                label="Password"
+                required
+              ></v-text-field>
+            </v-col>
+
+            <v-col
+              cols="12"
+              md="4"
+            >
+              <v-text-field
+                v-model="confirmpassword"
+                :rules="passwordRules"
+                :counter="10"
+                label="Confirme Password"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-form>
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'PageSetings',
+
+  components: {},
+
+  data: () => ({
+    username: 'John doe',
+    valid: false,
+    password: '',
+    confirmpassword: '',
+    passwordRules: [
+      v => !!v || 'password is required',
+      v => v.length <= 10 || 'Name must be less than 10 characters',
+    ],
+  }),
+
+  computed: {},
+
+  mounted () {},
+
+  methods: {}
+}
+</script>
